a2b56408936ccc687c0013999dd281f27dcd7ef0
Marecko <78494395+Marecko@users.noreply.github.com>
2025-01-15 13:57:23 +0100

    Level Loading

---
+++ main.py
@@ -0,0 +1,61 @@
+import csv
+
+from settings import *
+import sys
+from sprites import *
+from groups import AllSprites
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.all_sprites = AllSprites()
+        self.setup_map()
+
+    def setup_map(self):
+        with open('ExportedData.csv', mode='r') as file:
+            csv_reader = csv.reader(file)
+            map = []
+            for row in csv_reader:
+                map.append(row)
+        print(map)
+        for i in range(len(map)):
+            for j in range(len(map[i])):
+                if (map[i][j] == '0'):
+                    Sprite((j * TILE_SIZE, i * TILE_SIZE), "assets/farby/sprite_0.png", self.all_sprites)
+                if (map[i][j] == 'b'):
+                    Sprite((j * TILE_SIZE, i * TILE_SIZE), "assets/farby/sprite_3.png", self.all_sprites)
+                if (map[i][j] == '.'):
+                    Sprite((j * TILE_SIZE, i * TILE_SIZE), "assets/farby/sprite_5.png", self.all_sprites)
+                if(map[i][j] == 'r'):
+                    Sprite((j * TILE_SIZE, i * TILE_SIZE), "assets/farby/sprite_1.png", self.all_sprites)
+                if (map[i][j] == 'g'):
+                    Sprite((j * TILE_SIZE, i * TILE_SIZE), "assets/farby/sprite_2.png", self.all_sprites)
+                if (map[i][j] == 's'):
+                    Sprite((j * TILE_SIZE, i * TILE_SIZE), "assets/farby/sprite_4.png", self.all_sprites)
+
+    def run(self):
+        while self.running:
+            delta = self.clock.tick() / 1000
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.display_surface.fill('black')
+            self.all_sprites.draw([180,500])
+            self.all_sprites.update(delta)
+            pygame.display.update()
+            # self.clock.tick(60)
+
+        pygame.quit()
+        sys.exit()
+
+
+if __name__ == '__main__':
+    print("fuck")
+    game = Game()
+    game.run()
+
---
+++ sprites.py
@@ -0,0 +1,14 @@
+from settings import *
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self,position, file_path,groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(file_path).convert_alpha()
+        self.rect = self.image.get_rect(topleft=position)
+
+
+class CollisionSprite(pygame.sprite.Sprite):
+    def __init__(self,position,surface,groups):
+        super().__init__(groups)
+        self.image = surface
+        self.rect = self.image.get_rect(topleft = position)
